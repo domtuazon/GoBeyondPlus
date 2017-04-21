c6b3bfc9f640dd5fbb0b2a23604262c90c49f1de
Ubuntu <dominictuazon@PythonFlaskServer.ozfcogmh44yepkmucpgxjmt3pd.ix.internal.cloudapp.net>
2017-04-21 04:05:25 +0000

    Changed app.py for prod use

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 from flask_bower import Bower
 
 app = Flask(__name__)
+Bower(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    Bower(app)
+    app.run(host='0.0.0.0')
 
-Bower(app)
